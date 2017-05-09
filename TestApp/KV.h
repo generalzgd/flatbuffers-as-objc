1d69fb26a2b53505913df9681506c36cc94719d6
zhangguodong <zhangguodong@bianfeng.com>
2017-05-09 09:29:23 +0800

    rm

--- TestApp/KV.h
+++
@@ -1,6 +0,0 @@
-// automatically generated, do not modify
-
-@interface TestAppKV : Table 
-@property (nonatomic, assign)uint64_t key;
-@property (nonatomic, assign)double value;
-@end
--- TestApp/TestAppColor.h
+++
@@ -1,8 +0,0 @@
-// automatically generated, do not modify
-
-typedef NS_ENUM(NSInteger,  TestAppColor)  {
- TestAppRed = 1,
- TestAppGreen = 2,
- TestAppBlue = 3,
-};
-
